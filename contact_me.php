56deb4f0c2e0ca72716ea08e98af724b6cddd6bb
chocovanillaicecream <almondyseth.sato@gmail.com>
2018-03-15 08:24:21 +0900

    made a contact me function but having troubles with the mail function

---
+++ contact_me.php
@@ -0,0 +1,26 @@
+	<?php
+
+	echo "sending mail";
+	
+	$name = strip_tags(htmlspecialchars($_POST["name"]));
+	$email_address = strip_tags(htmlspecialchars($_POST["email"]));
+	$message = strip_tags(htmlspecialchars($_POST["message"]));
+	
+	echo $name; 
+	echo $email_address;
+	echo $message;   
+	// Create the email and send the message
+	$to = "almond.sato@coffeevanillaicecream.com"; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+	$email_subject = "Website Contact Form:  $name";
+	$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+	$headers = "From: noreply@coffeevanillaicecream.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+	$headers .= "Reply-To: $email_address";   
+	mail(
+     'almond.sato@coffeevanillaicecream.com',
+     'Works!',
+     'An email has been generated from your localhost, congratulations!');
+	//mail($to,$email_subject,$email_body,$headers);
+
+	echo "mail sent";
+	return true;         
+	?>
