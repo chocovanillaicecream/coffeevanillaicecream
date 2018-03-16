663a5df51833f8c68b0cd578ff993ca6ac2a3123
chocovanillaicecream <almondyseth.sato@gmail.com>
2018-03-17 02:48:59 +0900

    removed echoes for contact me page!

--- contact_me.php
+++ contact_me.php
@@ -1,6 +1,5 @@
 	<?php
-	echo "sending mail";
-	
+
 	$name = strip_tags(htmlspecialchars($_POST["name"]));
 	$email_address = strip_tags(htmlspecialchars($_POST["email"]));
 	$message = strip_tags(htmlspecialchars($_POST["message"]));
@@ -13,9 +12,7 @@
 	 contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 	$headers = "From: admin@coffeevanillaicecream.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 	$headers .= "Reply-To: $email_address";   
-	
 	mail($to,$email_subject,$email_body,$headers);
 
-	echo "mail sent";
 	return true;         
 	?>
