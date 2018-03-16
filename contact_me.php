ccdc309750795cbd9f174ef5b59ae91fe7a17f8b
chocovanillaicecream <almondyseth.sato@gmail.com>
2018-03-17 02:36:52 +0900

    removed checking and optimized some functions for mail()

--- contact_me.php
+++ contact_me.php
@@ -9,8 +9,9 @@
 	// Create the email and send the message
 	$to = "almond.sato@coffeevanillaicecream.com"; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 	$email_subject = "Website Contact Form:  $name";
-	$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-	$headers = "From: noreply@coffeevanillaicecream.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+	$email_body = "You have received a new message from coffeevanillaicecream.com
+	 contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+	$headers = "From: admin@coffeevanillaicecream.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 	$headers .= "Reply-To: $email_address";   
 	
 	mail($to,$email_subject,$email_body,$headers);
